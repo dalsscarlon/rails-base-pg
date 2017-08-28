0f8d44559425f3ad65b47670abdf8d9e0a471737
Dalia Carlon <daliamcc@gmail.com>
2017-08-28 12:06:55 -0600

    Set roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  enum role: %i[customer agent consultant admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :consultant
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
